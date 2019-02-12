c870ec4a2535e50f395e8b69e1c854144374415c
Fred Siika <fredsiika@gmail.com>
2019-02-11 23:54:29 -0800

    ft_memccpy.c complete.

---
+++ src/ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fredsiik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/11 23:51:33 by fredsiik          #+#    #+#             */
+/*   Updated: 2019/02/11 23:53:56 by fredsiik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t	i;
+	unsigned char *udst;
+	unsigned char *usrc;
+
+	i = 0;
+	udst = (unsigned char *)dst;
+	usrc = (unsigned char *)src;
+	while (i < n)
+	{
+		udst[i] = usrc[i];
+		if (usrc[i] == (unsigned char)c)
+			return (&udst[i + 1]);
+		i +=1;
+	}
+	return (NULL);
+}

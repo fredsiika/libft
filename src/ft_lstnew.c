6fdd243ebe5c2769a2bf6fe5b15a0cf4c72d1ac9
Fred Siika <fredsiika@gmail.com>
2019-02-28 22:33:22 -0800

    Added prototype `t_list *ft_lstnew`

--- src/ft_lstnew.c
+++ src/ft_lstnew.c
@@ -6,8 +6,30 @@
 /*   By: fredsiik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 22:24:25 by fredsiik          #+#    #+#             */
-/*   Updated: 2019/02/28 22:24:28 by fredsiik         ###   ########.fr       */
+/*   Updated: 2019/02/28 22:32:05 by fredsiik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+t_list	*ft_lstnew(void const *content, size_t constent_size)
+{
+	t_list *new;
+
+	if (!(new = (t_list *)malloc(sizeof(t_list) * content_size)))
+		return (NULL);
+	if (!content)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
+	else
+	{
+		if (!(new->content = (void *)malloc(content_size)))
+			return (NULL);
+		ft_memcpy(new->content, content, content_size);
+		new->content_size = content_size;
+	}
+	new->next = NULL;
+	return (new);
+}

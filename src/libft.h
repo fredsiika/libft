603586c8f969638b365329500776715c060dcf8d
Fred Siika <fredsiika@gmail.com>
2019-02-28 23:30:04 -0800

    Added prototypes for bonus functions.

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: fredsiik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 21:56:55 by fredsiik          #+#    #+#             */
-/*   Updated: 2019/02/28 22:37:46 by fredsiik         ###   ########.fr       */
+/*   Updated: 2019/02/28 23:28:02 by fredsiik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,10 @@ void			*ft_memalloc(size_t size);
 /*
  * Bonus functions
 */
-
+void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstnew(void const *content, size_t content_size);
-
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 #endif
